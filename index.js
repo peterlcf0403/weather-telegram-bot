3e6314188669b6a31d98d8035ce4de2642073ac4
Peter Lam <peterlcf0403@gmail.com>
2016-06-24 14:53:09 +0800

    'Topic' class implemented 'init()' function for server initialization is created

--- index.js
+++ index.js
@@ -3,19 +3,61 @@
 var Bot = require('node-telegram-bot-api');
 var feed = require('feed-read');
 
-// topic list
-var topics = ['current', 'warning'];
-
-// set up bot
 var token = '181337028:AAFfWt_1vivoBVwmC8K28ZRzmaegiedp1HM';
-var bot = new Bot(token, { polling: true });
+var bot;
+var topics = [];
 
-// command listeners
-// 'topic' command
-bot.onText(/^topic$/, function (msg, match){
-  console.log('[topic] command received: ' + match[0]);
-  bot.sendMessage(msg.chat.id, topics.join(', '));
-});
+// topic class
+function Topic(name, url, parser) {
+  this.name = name;
+  this.url = url;
+  this.data = 'No update for ' + this.name;
+  this.parser = parser.bind(this);
+}
 
+Topic.prototype = {
+  update: function() {
+    var topic = this;
+    feed(this.url, function(err, rss) {
+      console.log('['+ topic.name +'] rss fetched');
+      topic.parser(rss);
+      console.log(topic.data);
+    });
+  }
+};
 
-console.log('bot server started...');
+// server init
+function init() {
+  // create topic objects
+  topics.push( new Topic(
+    'current', 
+    'http://rss.weather.gov.hk/rss/CurrentWeather.xml', 
+    function(rss) {
+      var text = rss[0].content.replace(/<[^>]*>/g, '').split('\r\n');
+      text[3] = text[2].trim() + ' ' + text[3].trim();
+      this.data = text.slice(3,8).join('\n');
+    }
+  ));
+  
+  topics.push( new Topic(
+    'warning', 
+    'http://rss.weather.gov.hk/rss/WeatherWarningSummaryv2.xml', 
+    function(rss) {
+      this.data = rss[0].content.replace(/<[^>]*>/g, '');
+    }
+  ));
+  
+  // set up bot
+  var bot = new Bot(token, { polling: true });
+  
+  // adding listeners
+  // 'topic' command listener
+  bot.onText(/^topic$/, function (msg, match){
+    console.log('[topic] command received: ' + match[0]);
+    bot.sendMessage(msg.chat.id, topics.map(function(e){ return e.name }).join(', '));
+  });
+  
+  console.log('bot server started...');
+}
+
+init();
