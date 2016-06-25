548818e68fd9424296475716c14afa8a9ceff5dd
Peter Lam <peterlcf0403@gmail.com>
2016-06-25 16:27:34 +0800

    rss feed auto-update function implemented. Different topics can have different update handlers so that different update strategy can be applied.

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var bot = new Bot(token, { polling: true });
 var topics = [];
 
 // topic class
-function Topic(name, url, parser) {
+function Topic(name, url, parser, update) {
   
   // name of the topic used when subscribe & query
   this.name = name;
@@ -22,6 +22,9 @@ function Topic(name, url, parser) {
   
   // function to parse the feed to suitable format
   this.parser = parser.bind(this);
+  
+  // function to determine when to update the feed
+  this.update = update.bind(this);
   
   // stores the last feed
   this.data = 'No content available for ' + this.name;
@@ -39,22 +42,29 @@ Topic.prototype = {
   getFeed: function() {
     var topic = this;
     feed(this.url, function(err, rss) {
-      var date = rss[0].published;
       console.log('Received feed of topic ['+ topic.name +']...');
-      //console.log('\tDate: ' + date);
+      
+      // check if the rss feed is updated
+      var date = rss[0].published;
       if (topic.lastPublish != null && date.getTime() == topic.lastPublish.getTime()) {
         console.log('\tNo update.');
       } else {
-        topic.lastPublish = date;
-        topic.parser(rss);
         console.log('\tcontent updated!');
-        //console.log(rss);
+        
+        // parse rss feed and update publish time
+        topic.lastPublish = date;
+        if (typeof topic.parser == 'function')
+          topic.data = topic.parser(rss);
+        else
+          topic.data = rss[0].content;
         
         // send updates to chats
         topic.subs.forEach(function(id){
           bot.sendMessage(id, topic.data);
         });
       }
+      // update last feed time
+      topic.lastFeed = new Date();
     });
   },
   
@@ -65,9 +75,7 @@ Topic.prototype = {
       console.log('New subscriber to topic ['+ this.name +']');
       return true;
     }
-    else {
-      return false;
-    }
+    else return false;
   },
   
   // remove chat from topic's subscribe list
@@ -83,9 +91,11 @@ Topic.prototype = {
   }
 };
 
-// server init
+
+// server initialization
 function init() {
   // create topic objects
+  // topic 'current'
   topics.push( new Topic(
     'current', 
     'http://rss.weather.gov.hk/rss/CurrentWeather.xml', 
@@ -96,29 +106,51 @@ function init() {
       
       // reformat first line (time & location)
       var tmp = child[0].data.split('\r\n');
-      this.data = tmp[1].trim() + ' ' + tmp[2].trim();
+      var result = tmp[1].trim() + ' ' + tmp[2].trim();
       
       // process other lines, skips non-text childs
       for (let i=1; i<child.length; i++) {
         if (child[i].type == 'text') {
           let str = child[i].data.trim();
           if (str != '')
-            this.data += '\n   ' + str;
+            result += '\n   ' + str;
         }
       }
-      console.log(this.data);
+      console.log(result);
+      return result;
+    },
+    function() {
+      var currentTime = new Date();
+      var min = currentTime.getMinutes();
+      
+      // default interval for try getting current weather feed (10min)
+      var interval = 300000;
+      
+      // if its around 0 min, set a smaller interval
+      if (min < 10 || min > 50) interval = 30000;
+      if (currentTime.getTime() - this.lastFeed.getTime() > interval) {
+        this.getFeed();
+      }
     }
   ));
   
+  // topic 'warning'
   topics.push( new Topic(
     'warning', 
     'http://rss.weather.gov.hk/rss/WeatherWarningSummaryv2.xml', 
     function(rss) {
-      this.data = rss[0].content.replace(/<[^>]*>/g, '').trim();
+      return rss[0].content.replace(/<[^>]*>/g, '').trim();
+    },
+    function() {
+      var currentTime = new Date();
+      var interval = 30000;
+      if (currentTime.getTime() - this.lastFeed.getTime() > interval) {
+        this.getFeed();
+      }
     }
   ));
   
-  // adding listeners
+  // adding command listeners
   // 'topic' command listener
   bot.onText(/^topic$/, function (msg, match){
     console.log('[topic] command received: ' + match[0]);
@@ -169,7 +201,7 @@ function init() {
     }
   });
   
-  // update topics
+  // update topics' feed
   topics.forEach(function(topic) {
     topic.getFeed();
   });
@@ -179,10 +211,11 @@ function init() {
 
 init();
 
-// for debug
+// auto update topic's rss feed
 setInterval(function() {
   topics.forEach(function(topic) {
-    topic.getFeed();
+    if (typeof topic.update == 'function')
+      topic.update();
   });
 }, 10000);
 
